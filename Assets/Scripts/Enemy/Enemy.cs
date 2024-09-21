6ddaa0b238dd7dd31c78bf9f78555f046db9c193
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-09-21 00:07:27 -0500

    navmesh added

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,5 +5,41 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-   
+    [Header("Enemy Health and Damage")]
+    public float enemySpeed;
+
+    [Header("Enemy Things")]
+    public NavMeshAgent enemyAgent;
+    public Transform playerBody;
+    public LayerMask playerLayer;
+
+    [Header("Enemy States")]
+    public float visionRadius;
+    public float shootingRadius;
+    public bool playerInvisionRadius;
+    public bool playerInshootingRadius;
+    public bool iaPlayer = false;
+
+    private void Awake()
+    {
+        enemyAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerInvisionRadius = Physics.CheckSphere(transform.position, visionRadius, playerLayer);
+        playerInshootingRadius = Physics.CheckSphere(transform.position, shootingRadius, playerLayer);
+
+        if(playerInvisionRadius && !playerInshootingRadius)
+        PursuePlayer();
+    }
+
+    private void PursuePlayer()
+    {
+        if(enemyAgent.SetDestination(playerBody.position))
+        {
+            // animations
+        }
+    }
+
 }
