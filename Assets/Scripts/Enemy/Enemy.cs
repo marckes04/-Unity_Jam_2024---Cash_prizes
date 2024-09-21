5aa64827462da6c6bfd66007367e72619ca02294
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-09-21 01:00:04 -0500

    blood effects uploaded

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,12 +6,20 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     [Header("Enemy Health and Damage")]
+    public float giveDamage = 5f;
     public float enemySpeed;
 
     [Header("Enemy Things")]
     public NavMeshAgent enemyAgent;
+    public Transform Lookpoint;
+    public GameObject shootingRaycastArea;
     public Transform playerBody;
     public LayerMask playerLayer;
+
+
+    [Header("Enemy Shooting Var")]
+    public float timebtwshoot;
+    bool previouslyshoot;
 
     [Header("Enemy States")]
     public float visionRadius;
@@ -32,6 +40,8 @@ public class Enemy : MonoBehaviour
 
         if(playerInvisionRadius && !playerInshootingRadius)
         PursuePlayer();
+        if (playerInshootingRadius && playerInshootingRadius)
+            ShootPlayer();
     }
 
     private void PursuePlayer()
@@ -40,6 +50,31 @@ public class Enemy : MonoBehaviour
         {
             // animations
         }
+    }
+
+    private void ShootPlayer()
+    {
+        enemyAgent.SetDestination(transform.position);
+
+        transform.LookAt(Lookpoint);
+
+        if (!previouslyshoot)
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(shootingRaycastArea.transform.position, shootingRaycastArea.transform.forward, out hit,shootingRadius))
+            {
+                Debug.Log("Shooting" + hit.transform.name);
+            }
+        }
+
+        previouslyshoot = true;
+        Invoke(nameof(ActiveShooting), timebtwshoot);
+    }
+
+    private void ActiveShooting()
+    {
+        previouslyshoot = false;
     }
 
 }
