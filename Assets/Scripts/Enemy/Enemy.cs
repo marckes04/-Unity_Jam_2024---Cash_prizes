a4aab46251248090ab4c44375611f05870fe69d4
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2025-12-26 03:03:28 +0100

    enemy AI

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Enemy Health and Damage")]
+    public float enemySpeed;
+
+    [Header("Enemy Things")]
+    public NavMeshAgent enemyAgent;
+    public Transform playerBody;
+    public LayerMask playerLayer;
+
+    [Header("Enemy States")]
+    public float visionRadius;
+    public float shootingRadius;
+    public bool playerInvisionRadius;
+    public bool playerInShootingRadius;
+    public bool IsPlayer = false;
+
+    void Awake()
     {
-        
+        enemyAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        playerInvisionRadius = Physics.CheckSphere(transform.position,visionRadius, playerLayer);
+        playerInShootingRadius = Physics.CheckSphere(transform.position, shootingRadius, playerLayer);
+
+        if(playerInvisionRadius && !playerInShootingRadius) PursuePlayer();
+    }
+
+    private void  PursuePlayer()
+    {
+        if(enemyAgent.SetDestination(playerBody.position))
+        {
+            //Animations
+        }
     }
 }
