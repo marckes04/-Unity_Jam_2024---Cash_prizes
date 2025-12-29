1501622a978a611861b4cd41110a8e52de5033c2
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2025-12-29 03:25:19 +0100

    enemy state shoot Added

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,12 +6,19 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     [Header("Enemy Health and Damage")]
+    public float giveDamage = 5f;
     public float enemySpeed;
 
     [Header("Enemy Things")]
     public NavMeshAgent enemyAgent;
+    public Transform lookPoint;
+    public GameObject shootingRaycastArea;
     public Transform playerBody;
     public LayerMask playerLayer;
+
+    [Header("Enemy Shooting Var")]
+    public float timebtwShoot;
+    bool previouslyShoot;
 
     [Header("Enemy States")]
     public float visionRadius;
@@ -31,6 +38,7 @@ public class Enemy : MonoBehaviour
         playerInShootingRadius = Physics.CheckSphere(transform.position, shootingRadius, playerLayer);
 
         if(playerInvisionRadius && !playerInShootingRadius) PursuePlayer();
+        if (playerInvisionRadius && playerInShootingRadius) ShootPlayer();  
     }
 
     private void  PursuePlayer()
@@ -39,5 +47,28 @@ public class Enemy : MonoBehaviour
         {
             //Animations
         }
+    }
+
+    private void ShootPlayer()
+    {
+        enemyAgent.SetDestination(transform.position);
+
+        transform.LookAt(lookPoint);
+
+        if(!previouslyShoot)
+        {
+            RaycastHit hit;
+            
+            if(Physics.Raycast(shootingRaycastArea.transform.position, shootingRaycastArea.transform.forward, out hit, shootingRadius))
+            {
+
+            }
+        }
+        previouslyShoot = true;
+        Invoke(nameof(ActiveShooting), timebtwShoot);
+    }
+    private void ActiveShooting()
+    {
+        previouslyShoot = false;
     }
 }
